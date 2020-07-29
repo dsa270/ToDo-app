7130cdcf68c153a1a68bb8d230c7a2bb4e33ab1f
Your Name <you@example.com>
2020-07-29 21:22:08 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
+import ToDolist from './ToDolist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [inputList,setinputList] = useState("");
+const [items,setitems] = useState([]);
+
+const itenEvent=(event) =>{
+  setinputList(event.target.value);
+};
+
+const listOfItems=() =>{
+setitems((oldItems) =>{
+  return [...oldItems, inputList];
+});
+setinputList("");
+};
+
+const DeleteItem=(id) =>{
+  console.log('deleted');
+
+setitems((oldItems)=>{
+    return oldItems.filter((arrElem, index)=>{
+         return index !==id;      
+    });
+  });
+};
+
+ return <>
+<div className="main_div">
+<div className="center_div">
+<br/>
+<h1>ToDo list</h1>
+<br/>
+<input type="text" placeholder="add an item"
+value={inputList}
+ onChange={itenEvent}></input>
+<button onClick={listOfItems}> + </button>
+<ol>
+  
+{items.map((itemval, index) => {
+ return <ToDolist key={index} 
+ id={index}
+ text= {itemval}
+   onSelect={DeleteItem}
+ />;
+})}
+
+</ol>
+</div>
+</div>
+</>;
 }
 
 export default App;
